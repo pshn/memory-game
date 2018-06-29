dd8012078ab7eaef2be7f28ca23067de5ca9bb5c
sylveelee <pshn0530@gmail.com>
2018-06-29 15:01:30 -0400

    Shows and counter increments after selecting cards

--- js/app.js
+++ js/app.js
@@ -53,12 +53,21 @@ function shuffle(array) {
  *  - if the list already has another card, check to see if the two cards match ✅
  *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one) ✅
  *    + if the cards do not match, remove the cards from the list ✅ and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
+ *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one) ✅
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
 
+ let counter = 0;
+ let moves = document.querySelector('.moves');
+ moves.innerHTML = counter;
+
  let openCardsArray = [];
  let cardsArray = document.querySelectorAll('li.card');
+
+ function increaseCounter(){
+   counter++;
+   moves.innerHTML = counter;
+ }
 
  function clearCardsList(a){
    a.length = 0;
@@ -91,6 +100,7 @@ function shuffle(array) {
    this.classList.add('open', 'show');
    openCardsArray.push(this);
    if(openCardsArray.length === 2){
+     increaseCounter();
      compareTwoCards(openCardsArray);
    }
  }
