ee83d82e6c010be6540f69759166a0a74f1dd516
sylveelee <pshn0530@gmail.com>
2018-06-29 12:14:25 -0400

    Have two cards to be displayed and compared

--- js/app.js
+++ js/app.js
@@ -48,9 +48,9 @@ function shuffle(array) {
 
 /*
  * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
+ *  - display the card's symbol (put this functionality in another function that you call from this one) ✅
+ *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one) ✅
+ *  - if the list already has another card, check to see if the two cards match ✅
  *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
  *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
@@ -58,9 +58,22 @@ function shuffle(array) {
  */
 
  let cardsArray = document.querySelectorAll('li.card');
+ let arr = [];
+
+ function compareTwoCards(a){
+   if (a[0].firstChild.className === a[1].firstChild.className){
+     console.log('yes');
+   } else {
+     console.log('no');
+   }
+   console.log('what\'s this', a[0].firstChild.className);
+   console.log('what\'s this', a[1].firstChild.className);
+ }
 
  function revealCard(){
    this.classList.add('open', 'show');
+   arr.push(this);
+   if(arr.length == 2){ compareTwoCards(arr); }
  }
 
  cardsArray.forEach(function(c){
